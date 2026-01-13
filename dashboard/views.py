fa568aa6e8326ab11a0575562a15a6759643230c
DanRCM <112514991+DanRCM@users.noreply.github.com>
2026-01-13 03:33:59 +0000

    Respuesta de APIs externas

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.conf import settings  # Para acceder a API_URL
+import requests                   # Para hacer la petición
 
 def index(request):
-    # Creamos un diccionario llamado 'data'
-    # La clave 'title' será el nombre de la variable en el HTML
+    # 1. Realizar la solicitud GET a la API externa
+    response = requests.get(settings.API_URL)
+    
+    # 2. Convertir la respuesta (texto) a una lista de diccionarios (JSON)
+    posts = response.json()
+
+    # 3. Calcular el dato que queremos mostrar (cantidad de posts)
+    total_responses = len(posts)
+
+    # 4. Preparar el contexto
     data = {
         'title': "Landing Page Dashboard",
+        'total_responses': total_responses, # Agregamos el nuevo dato
     }
 
-    # Pasamos 'data' como tercer argumento a render
     return render(request, 'dashboard/index.html', data)
