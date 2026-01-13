ee4ce23615ca335e38cb9c2a73979bf8f0501881
DanRCM <112514991+DanRCM@users.noreply.github.com>
2026-01-13 03:25:37 +0000

    Datos del lado del servidor

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-def index(request): 
-    return render(request, 'dashboard/index.html')
+def index(request):
+    # Creamos un diccionario llamado 'data'
+    # La clave 'title' será el nombre de la variable en el HTML
+    data = {
+        'title': "Landing Page Dashboard",
+    }
+
+    # Pasamos 'data' como tercer argumento a render
+    return render(request, 'dashboard/index.html', data)
