8463a92961a1944732cb368d2e8bd70f43500af4
sizaguir <138946181+sizaguir@users.noreply.github.com>
2026-01-17 20:22:28 +0000

    Se añadio soporte a mysql

--- backend_analytics_server/settings.py
+++ backend_analytics_server/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import os
 import pymysql
 
+pymysql.version_info = (2, 2, 2, "final", 0) # <--- ¡Esta es la clave!
 pymysql.install_as_MySQLdb()
 # --- FIN DEL CAMBIO ---
 
